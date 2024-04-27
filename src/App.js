d0a94a57ca57d43e9d1815ddd0cddecbb8afec0f
kouhei <kouhei.basketball7.8@gmail.com>
2024-04-27 13:49:47 +0900

    add:propsを受け取り、セレクトボックスで表示する機能追加

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
-
+const App = ({ practices }) => {
   return (
     <div className="App">
       <header className="App-header">
-
+        <select className="App-select">
+          {practices.map((name, index) => (
+            <option key={name} value={index}>
+              {name}
+            </option>
+          ))}
+        </select>
       </header>
     </div>
   );
-}
+};
 
 export default App;
