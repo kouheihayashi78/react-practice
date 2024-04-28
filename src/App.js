80e2b3fcb4293dc8b6d3805ea8c3b5fff341d39f
kouhei <kouhei.basketball7.8@gmail.com>
2024-04-28 12:47:11 +0900

    add:lazy, Suspense, useEffectを用いて、プルダウン選択時に選択したページが読み込まれて表示される機能を実装

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import { useState } from "react";
+import { useState, lazy, Suspense, useEffect } from 'react';
 import './App.css';
 
-const App = ({ practices }) => {
-  const [practiceIndex, setPracticeIndex] = useState(0);
+const Loader = ({ practice }) => {
+  // lazyをコンポーネントの外部で呼び出し、遅延読み込みされるReactコンポーネントを宣言
+  const LazyComponent = lazy(() => import(`./${practice}`));
 
+  return (
+    // Suspenseはコンポーネント内で非同期な操作完了するまで、ローディング状態を管理（遅延）する
+    <Suspense fallback={<div>読み込み中...</div>}>
+      <LazyComponent />
+    </Suspense>
+  );
+};
+
+const App = ({ practices }) => {
+  let restoredLecIndx = 0;
+  restoredLecIndx = restoredLecIndx < practices.length ? restoredLecIndx : 0;
+  const [practiceIndex, setPracticeIndex] = useState(restoredLecIndx);
+
+  // practice.jsの配列のページ名を取得
+  const practiceName = practices[practiceIndex];
+
+  // プルダウンからページ名を選択すると発火
   const contentChange = (e) => {
-    setPracticeIndex(e.target.value)
-  }
+    setPracticeIndex(e.target.value);
+  };
+
+  // useEffectは関数の実行タイミングをReactのレンダリング後まで遅らせるhook
+  // useEffectの第一引数は関数、第二引数は配列
+  // setItemのメソッドの引数（keyName: string, keyValue: string）
+  useEffect(() => localStorage.setItem('items', JSON.stringify(practiceIndex)),[practiceIndex]);
 
   return (
     <div className="App">
@@ -18,6 +41,9 @@ const App = ({ practices }) => {
             </option>
           ))}
         </select>
+        <div className="App-content">
+          <Loader practice={`${practiceName}`} />
+        </div>
       </header>
     </div>
   );
