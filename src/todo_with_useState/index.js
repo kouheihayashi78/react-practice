3cb65babc380e555f6656522fbbbd34eb840627f
kouhei <kouhei.basketball7.8@gmail.com>
2024-05-04 17:03:56 +0900

    add:todoリストを実装する前に大まかに要素を実装

--- src/todo_with_useState/index.js
+++ src/todo_with_useState/index.js
@@ -1,10 +1,30 @@
 
 const todoState = () => {
+    const tasks = [
+        '日記',
+        '学習記録つける',
+        'react楽しい!'
+    ]
+
+    const clickTask = () => {
+        console.log('クリックされました')
+    }
+    
     return(
+        // <>は<React.Fragment>の省略形
         <>
-            <h1>todo_with_useState</h1>
+            <h3 className='todo-title'>シンプルなtodoリスト(useState版)</h3>
+            {tasks.map(task => {
+                return (
+                    // keyを指定することでReactは各要素を一意に識別する
+                    <div className="todo-content" key={task}>
+                        <button type="checkbox" onClick={clickTask}>完了</button>
+                        <span>{task}</span>
+                    </div>
+                )
+            })}
         </>
     )
 }
 
-export default todoState
+export default todoState;
