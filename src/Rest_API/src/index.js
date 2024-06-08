73345d6576a4e5c44c67d8644a3c5afbf434bb3c
kouhei <kouhei.basketball7.8@gmail.com>
2024-06-08 15:14:54 +0900

    fix:【コミット忘れ】axiosを使ってRest APIでサーバーからデータを取得するための準備

---
+++ src/Rest_API/src/index.js
@@ -0,0 +1,11 @@
+const Example = () => {
+  return (
+    <>
+      <h3 className="todo-title">
+        todoリスト（Rest API）
+      </h3>
+    </>
+  );
+};
+
+export default Example;
--- src/practices.js
+++ src/practices.js
@@ -1,7 +1,8 @@
 const practices = [
     "todo_with_useState",
     "todo_with_useReducer",
-    "redux_Toolkit"
+    "redux_Toolkit",
+    "Rest_API/src",
 ]
     
 export default practices;
