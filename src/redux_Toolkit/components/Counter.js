ded9b06e01a9caaa37b57c36055b1557c395ca1b
kouhei <kouhei.basketball7.8@gmail.com>
2024-06-08 15:09:28 +0900

    fix:【コミット忘れ】非同期で値を減算する処理追加

--- src/redux_Toolkit/components/Counter.js
+++ src/redux_Toolkit/components/Counter.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux"
-import { add, minus, addAsyncWithStatus } from "../store/modules/counter"
+import { add, minus, addAsyncWithStatus, minusAsyncWithStatus } from "../store/modules/counter"
 
 import CounterResult from "./CounterResult"
 import CounterButton from "./CounterButton"
@@ -12,6 +12,7 @@ const Counter = () => {
             <CounterButton step={2} calcType="+" actionCreator={add}/>
             <CounterButton step={2} calcType="-" actionCreator={minus}/>
             <CounterButton step={2} calcType="(非同期) +" actionCreator={addAsyncWithStatus}/>
+            <CounterButton step={2} calcType="(非同期) -" actionCreator={minusAsyncWithStatus}/>
             <h3>{status}</h3>
         </>
     )
--- src/redux_Toolkit/index.js
+++ src/redux_Toolkit/index.js
@@ -5,7 +5,7 @@ import store from "./store"
 const Example = () => {
   return (
     <Provider store={store}>
-      <h3 className="todo-title">簡単なカウントアップ機能(非同期処理も実装)</h3>
+      <h3 className="todo-title">reduxを用いた簡単なカウントアップ機能(非同期処理も実装)</h3>
       <Counter />
     </Provider>
   );
